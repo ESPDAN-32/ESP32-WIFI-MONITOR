0b9ab53ea6a81af70a631d3a363d27129ab84f4d
ESPDAN <cphr253@gmail.com>
2026-05-28 17:09:58 +0300

    User_setup.h

---
+++ User_setup.h
@@ -0,0 +1,28 @@
+#define ST7735_DRIVER
+
+#define TFT_WIDTH 128
+#define TFT_HEIGHT 160
+
+#define ST7735_REDTAB_160x128
+
+#define TFT_OFFSET_X 1
+#define TFT_OFFSET_Y 2
+
+#define TFT_CS 5
+#define TFT_DC 2
+#define TFT_RST 4
+
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_MISO 19
+
+#define SPI_FREQUENCY 10000000
+
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define LOAD_FONT6
+#define LOAD_FONT7
+#define LOAD_FONT8
+#define LOAD_GFXFF
+#define SMOOTH_FONT
